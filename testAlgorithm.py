6dadc03f323b08eb21160843d520308b8ef7470e
mateusz.roszkowski <mateusz.roszkowski@asseco.pl>
2023-12-13 21:46:17 +0100

    dodanie folium

--- testAlgorithm.py
+++ testAlgorithm.py
@@ -4,6 +4,9 @@ import geopandas as gpd
 from sqlalchemy import create_engine
 from shapely.geometry import Point, LineString
 import pyproj
+import matplotlib.pyplot as plt
+import folium
+
 
 # Helper function to find the nearest node in the graph
 def nearest_node(lon, lat, graph):
@@ -30,8 +33,8 @@ engine = create_engine(db_uri)
 # Define place A and place B coordinates (in SRID 4326 - WGS84)
 lonA = 20.85950
 latA = 52.19354
-lonB = 20.80745
-latB = 52.18021
+lonB = 19.4897
+latB = 51.801
 
 # Convert place A and place B coordinates to SRID 3857 (Web Mercator)
 transformer = pyproj.Transformer.from_crs(4326, 3857, always_xy=True)
@@ -40,7 +43,7 @@ lonA_3857, latA_3857 = transformer.transform(lonA, latA)
 lonB_3857, latB_3857 = transformer.transform(lonB, latB)
 
 # Buffer distance around place A and place B to create a subset (adjust as needed)
-buffer_distance = 100  # in meters
+buffer_distance = 10000  # in meters
 
 # Create a bounding box around place A and place B in SRID 3857
 bbox = (
@@ -75,17 +78,29 @@ nodeB = nearest_node(lonB_3857, latB_3857, G)
 
 # Find the shortest path
 shortest_path = nx.shortest_path(G, source=nodeA, target=nodeB, weight='weight')
-
+# shortest_path_graph = G.subgraph(shortest_path)
+print("ok")
 # Print the names of roads in the shortest path
-for i in range(len(shortest_path) - 1):
-    start_point = Point(shortest_path[i])
-    end_point = Point(shortest_path[i + 1])
-    road_segment = LineString([start_point, end_point])
+# for i in range(len(shortest_path) - 1):
+#     start_point = Point(shortest_path[i])
+#     end_point = Point(shortest_path[i + 1])
+#     road_segment = LineString([start_point, end_point])
+#     matching_row = gdf[gdf['way'] == road_segment]
+#     if not matching_row.empty:
+#         name = matching_row.iloc[0]['name']
+#         print(f"Road Name: {name}")
 
-    matching_row = gdf[gdf['way'] == road_segment]
-    if not matching_row.empty:
-        name = matching_row.iloc[0]['name']
-        print(f"Road Name: {name}")
+map_center = [(latA + latB) / 2, (lonA + lonB) / 2]
+m = folium.Map(location=map_center, zoom_start=9)
+
+transformer = pyproj.Transformer.from_crs(3857, 4326)
+shortest_path_coords = [transformer.transform(coord[0], coord[1]) for coord in shortest_path]
+
+# Add the shortest path as a PolyLine to the map
+folium.PolyLine(locations=shortest_path_coords, color='blue', weight=5).add_to(m)
+
+# Save the map to an HTML file or display it
+m.save("shortest_path_map.html")
 
 # Close the SQLAlchemy engine
 engine.dispose()
